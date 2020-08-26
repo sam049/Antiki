6fe128122336b5b3c50cdfb608618f9082b87898
Sam Miller <sammillerks@gmail.com>
2020-08-26 12:34:07 -0400

    testing a2 deployment

--- client/components/singleItem.js
+++ client/components/singleItem.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import {render} from 'enzyme'
+import axios from 'axios'
 
 export default function SingleItem(props) {
+  console.log('>>>>>', props.isAdmin)
   return (
     <div>
       {props.name + ' '}
       {props.description}
       <img src={props.imageUrl} className="img" />
+      {props.isAdmin ? (
+        <button
+          onClick={() => {
+            axios.delete(`/api/items/${props.id}`)
+          }}
+        >
+          {' '}
+          X
+        </button>
+      ) : null}
     </div>
   )
 }
